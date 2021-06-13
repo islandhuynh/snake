5d51ddead90a0f9436bc7387569b0319b7f2c85d
Island <islandhuynh@gmail.com>
2021-06-13 01:40:52 -0400

    added key listening, allowing users to change direction of the snake

--- main.py
+++ main.py
@@ -2,13 +2,37 @@ from turtle import Turtle, Screen
 import random
 
 screen = Screen()
-screen.setup(width=500, height=400)
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game by Island Huynh")
 
 snake = Turtle("square")
 snake.color("green")
+snake.speed(1)
 
 food = Turtle("square")
 food.color("red")
 food.goto(x=150, y=150)
+
+def move_up():
+  snake.setheading(90)
+
+def move_down():
+  snake.setheading(270)
+
+def move_left():
+  snake.setheading(180)
+
+def move_right():
+  snake.setheading(0)
+
+screen.onkey(move_up, "w")
+screen.onkey(move_down, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.listen()
+
+while snake.xcor() < 300 and snake.xcor() > -300 and snake.ycor() < 300 and snake.ycor() > -300:
+  snake.forward(10)
 
 screen.exitonclick()
