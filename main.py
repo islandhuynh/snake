bf02f251a36aa4eb2e51e0b5b822cb952a85c862
Island <islandhuynh@gmail.com>
2021-06-21 23:39:40 -0400

    Added high score and snake now resets when game is over instead of force quitting the game

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ game_is_on = True
 
 while game_is_on:
   if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-    score.game_over()
-    game_is_on = False
+    score.reset()
+    snake.reset()
   screen.update()
   time.sleep(0.1)
   snake.move()
@@ -35,7 +35,7 @@ while game_is_on:
     score.increase_score()
   for seg in snake.segments[1:]:
     if snake.head.distance(seg) < 10:
-      score.game_over()
-      game_is_on = False
+      score.reset()
+      snake.reset()
 
 screen.exitonclick()
